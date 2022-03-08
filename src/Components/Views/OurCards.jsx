0e4bae3c98081268b36f046b93e6d474d614c917
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-03-08 12:41:31 +0100

    CARDS PAGE

--- src/Components/Views/OurCards.jsx
+++ src/Components/Views/OurCards.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import Nav from './Nav'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import '../Views/personal.scss'
+import Footer from '../../Footer'
 function OurCards() {
   return (
-    <div>OurCards</div>
+    <div className='personal'>
+      <Nav/>
+      <div className="firstSec">
+        <div className="left">
+          <h1>ORANGE Personal Banking Cards</h1>
+          <p>With your cards you are a TITAN, anywhere and everywhere you go.</p>
+        </div>
+      </div>
+
+      <div className="secondSection">
+        <div className="top">
+        <h1>ORANGE Cards</h1>
+        <p>With your cards you are a GIANT, anywhere and everywhere you go.</p>
+        </div>
+
+        <div className="bottom">
+          <div className="cards">
+          <div className="circle">
+            <FontAwesomeIcon className='ico' icon={faDatabase}></FontAwesomeIcon>
+          </div>
+          <h1>Current Account</h1>
+          <p>Banking the way you want it - flexible and convenient with lots of benefits.</p>
+            
+          </div>
+        </div>
+      </div>
+
+      <Footer/>
+    </div>
   )
 }
 
